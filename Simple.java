41612fd0f1d4a905231164ab0a0e546806588074
Salah-ELdeen-Alqayem <152782320+Salah-ELdeen-Alqayem@users.noreply.github.com>
2025-05-27 00:20:55 +0300

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,25 @@
+/*salah-ELdenn*/
+
+package com.leecottrell.simpleclasstest;
+
+
+public class Simple {
+
+    public int addEm (int a , int b) {
+
+return a+b;
+}
+
+public int subEm (int a , int b) {
+
+return a-b;
+}
+
+public float divEm (int a , int b) {
+float ans =a/b;
+return ans;
+}
+    public static void main(String[] args) {
+        System.out.println("اHALAH ");
+    }
+}
